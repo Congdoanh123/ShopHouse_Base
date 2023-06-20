0bd5a84cd82e15e6254976e17f3f958ab4d8f5c2
macos <nguyencongdoanh19@gmail.com>
2023-06-20 09:47:04 +0700

    init

---
+++ ShopHouse_Base/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ShopHouse_Base.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
